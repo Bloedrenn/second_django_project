182b11c2a0076f5369d255c8edf9b02e079dc152
Bloedrenn <bloedrenn@gmail.com>
2024-01-21 19:18:37 +0300

    commit #2

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import say_hello, say_main
+from .views import say_hello, say_main, get_posts
+
+app_name = 'main'
 
 urlpatterns = [
     path('', say_main),
-    path('hello/', say_hello)
+    path('hello/', say_hello),
+    path('posts/', get_posts, name='get_posts')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+menu = [
+    {'title': 'Посты', 'path_name': 'main:get_posts'},
+    # {'title': 'Создать пост', 'path_name': 'main:add_post'},
+    # {'title': 'Контакты', 'path_name': 'main:contacts'},
+    # {'title': 'О сайте', 'path_name': 'main:about'}
+]
 
 
 def say_hello(request):
@@ -9,3 +17,11 @@ def say_hello(request):
 
 def say_main(request):
     return render(request, 'main/main.html')
+
+
+def get_posts(request):
+    posts = Post.objects.all()
+
+    context = {'posts': posts, 'menu': menu}
+
+    return render(request, template_name='main/posts.html', context=context)
